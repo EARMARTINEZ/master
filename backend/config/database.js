ed1a09ad5290993e05f361e3778f4f139feb754e
Emmanuel Ruiz <earmartinez@gmail.com>
2023-11-13 16:04:17 -0400

    up

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,12 @@
  module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', 'database-clientapi.cyf9zngcrkjn.us-east-1.rds.amazonaws.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'masterepk'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'p7JxjkbeIJ5piSnOJ038'),
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'bd_devmaster'),
+      user: env('DATABASE_USERNAME', 'master'),
+      password: env('DATABASE_PASSWORD', '9K-KU71Y7e*WYzIxh+^RTwu6+gzjVQC^'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
