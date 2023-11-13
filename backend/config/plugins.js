57aab16225d1a84e13a74384bd769cd71e7f99e0
Emmanuel Ruiz <earmartinez@gmail.com>
2023-11-13 19:45:57 -0400

    up

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -7,23 +7,7 @@ module.exports = ({ env }) => ({
         },
       },
     },
-    // ...
-   // ...
- /*  email: {
-    config: {
-      provider: 'sendgrid',
-      providerOptions: {
-        apiKey: process.env.SENDGRID_API_KEY,
-      },
-      settings: {
-        defaultFrom: 'earmartinez@gmail.com',
-        defaultReplyTo: 'earmartinez@gmail.com',
-       
-      },
-    },
-  }, */
-  // ...
-
+   
 
   email: {
     config: {
