c1d11379c9879e3b4305fe8d4c9da01c93ee6c09
Emmanuel Ruiz <earmartinez@gmail.com>
2023-11-13 16:06:21 -0400

    up

--- backend/config/database.js
+++ backend/config/database.js
@@ -11,18 +11,3 @@
     },
   },
 }); 
-
-/* module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', '104.194.11.32'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'koisite'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '9O3eci0egq64L7VmgrE3'),
-      ssl: env.bool('DATABASE_SSL', false),
-    },
-  },
-});
- */
