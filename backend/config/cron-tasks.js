c6a591fce036e2fe3efc302e6a7440a8a3fa5294
Emmanuel Ruiz <earmartinez@gmail.com>
2024-01-09 20:03:30 -0400

    up

--- backend/config/cron-tasks.js
+++ backend/config/cron-tasks.js
@@ -153,7 +153,7 @@ module.exports = {
                 
                        
                   collection: {         
-                     id: 18,
+                     id: 30,
                   },
                               
          },
