2b3d139a7cc63f283b16b6bc87e14f7de199ba3e
Emmanuel Ruiz <earmartinez@gmail.com>
2024-03-01 10:51:14 -0400

    up

--- frontend/src/components/Combinations/CombinationView.jsx
+++ frontend/src/components/Combinations/CombinationView.jsx
@@ -1,33 +1,18 @@
-
-import {CombinationEje1} from '@/components/Combinations/CombinationEje1'
-
-import {CombinationEje2} from '@/components/Combinations/CombinationEje2'
 import {CombinationBoard} from '@/components/Combinations/CombinationBoard'
 
-import {CombinationEje3} from '@/components/Combinations/CombinationEje3'
-import {CombinationEje4} from '@/components/Combinations/CombinationEje4'
-
-import Board from '@/components/Combinations/dnd/board/Board'
-import { generateQuoteMap } from './dnd/mockData';
 
 export function CombinationView() {   
     
-    const data = {
-        medium: generateQuoteMap(10),
-        large: generateQuoteMap(500),
-      };
-
-      //console.log(data.medium)
+   
     
   return (
     
   <>
  
- {/* <CombinationEje1 />  */}
 
  <CombinationBoard />  
 
- {/* <Board initial={data.medium} withScrollableColumns />         */}
+
    
   </>
 
